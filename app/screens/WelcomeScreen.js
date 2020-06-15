588f4e21fe4ad28ebc11d635d00c061baeb3061f
David Santana <davidsantana409@yahoo.com>
2020-06-15 16:23:52 -0700

    Added butons#

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -6,12 +6,27 @@ export default function WelcomeScreen() {
     <ImageBackground
       style={styles.background}
       source={require("../assets/wScreen.jpg")}
-    ></ImageBackground>
+    >
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: "flex-end",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
   },
 });
