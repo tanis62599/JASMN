aca9013ab27da31ec5964c5bb0286450451e4ddc
Tanis <tyusicosuarez1999@gmail.com>
2020-06-17 13:50:03 -0700

    PushForNow

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import SignUpScreen from "./app/screens/SignUpScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <SignUpScreen />;
 }
