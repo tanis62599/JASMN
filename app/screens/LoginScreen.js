6ef07c500094e178d87511b394cf9257876f57d6
David Santana <davidsantana409@yahoo.com>
2020-06-17 17:41:26 -0700

    Added 2 new files

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { ImageBackground, StyleSheet, Text, onChangeText } from "react-native";
+
+class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <Text>{"Hello"}</Text>;
+  }
+}
+
+export default LoginScreen;
